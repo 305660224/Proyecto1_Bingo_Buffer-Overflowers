e0fadb52b6768df002986272469f6074fdc370b7
Glorana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-11-12 20:15:25 -0600

    Se cambia el enum aplicando el patron strategy

---
+++ src/Modelo/EnumModoJuego.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author huete
+ */
+public enum EnumModoJuego {
+   
+    NORMAL("Juego Normal", new ReglaNormal()),
+    CUATRO_ESQUINAS("Cuatro Esquinas", new ReglaCuatroEsquinas()),
+    CARTON_LLENO("Cartón Lleno", new ReglaCartonLlenol());
+    
+
+    //variable
+    private String modoJuego;
+    //regla  aplicando strategy
+    private final ReglaGanadora regla;
+    
+    //constructor
+    EnumModoJuego(String modoJuego, ReglaGanadora regla) {
+        this.modoJuego = modoJuego;
+        this.regla = regla;
+    }
+    
+    //metodo getter
+    public ReglaGanadora getRegla(){
+        return regla;
+    }
+    
+    //metodo getter
+    public String getModoJuego() {
+        return modoJuego;
+    }
+    
+    //usado para pruebas
+    @Override
+    public String toString() {
+        return modoJuego;
+    }
+}
